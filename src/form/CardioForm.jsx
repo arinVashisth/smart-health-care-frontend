95253aa7e812a5c8e3827fa7fa43e30c672938e6
Arin Vashisth <157912485+arinVashisth@users.noreply.github.com>
2025-07-09 14:08:25 +0530

    Update CardioForm.jsx

--- src/form/CardioForm.jsx
+++ src/form/CardioForm.jsx
@@ -72,7 +72,7 @@ const Tooltip = ({ text }) => (
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/predict/cardiovascular', {
+      const res = await fetch('https://smart-health-care-tracker-backend.onrender.com/predict/cardiovascular', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
